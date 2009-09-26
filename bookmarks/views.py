7b977ee4e7bbbdf592c8b0363400dabcad5dfcc0
kevin fricovsky <kevin@montylounge.com>
2009-09-27 04:03:50 +0800

    added new template override arg, and added detail view

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -13,17 +13,35 @@ from django.utils.translation import ugettext_lazy as _
 
 from bookmarks.models import Bookmark, BookmarkInstance
 from bookmarks.forms import BookmarkInstanceForm
+from django.views.generic import date_based
 
-def bookmarks(request):
+def bookmarks(request, template_name='bookmarks/bookmarks.html'):
     bookmarks = Bookmark.objects.all().order_by("-added")
     if request.user.is_authenticated():
         user_bookmarks = Bookmark.objects.filter(saved_instances__user=request.user)
     else:
         user_bookmarks = []
-    return render_to_response("bookmarks/bookmarks.html", {
+    return render_to_response(template_name, {
         "bookmarks": bookmarks,
         "user_bookmarks": user_bookmarks,
     }, context_instance=RequestContext(request))
+
+
+def bookmark_detail(request, slug, year, month, day, template_name='bookmarks/bookmark_detail.html', **kwargs):
+    '''Detail bookmark view.'''
+
+    return date_based.object_detail(
+        request,
+        year = year,
+        month = month,
+        day = day,
+        date_field = 'added',
+        slug = slug,
+        queryset = Bookmark.objects.all(),
+        template_object_name='bookmark',
+        template_name=template_name,
+        **kwargs
+    )
 
 
 @login_required
@@ -35,7 +53,7 @@ def your_bookmarks(request):
 
 @login_required
 def add(request):
-    
+
     if request.method == "POST":
         bookmark_form = BookmarkInstanceForm(request.user, request.POST)
         if bookmark_form.is_valid():
@@ -43,7 +61,7 @@ def add(request):
             bookmark_instance.user = request.user
             bookmark_instance.save()
             bookmark = bookmark_instance.bookmark
-            
+
             try:
                 headers = {
                     "Accept" : "text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5",
@@ -57,11 +75,11 @@ def add(request):
                 has_favicon = True
             except:
                 has_favicon = False
-            
+
             bookmark.has_favicon = has_favicon
-            bookmark.favicon_checked = datetime.now() 
+            bookmark.favicon_checked = datetime.now()
             bookmark.save()
-            
+
             if bookmark_form.should_redirect():
                 return HttpResponseRedirect(bookmark.url)
             else:
@@ -75,15 +93,15 @@ def add(request):
             initial["description"] = request.GET["description"].strip()
         if "redirect" in request.GET:
             initial["redirect"] = request.GET["redirect"]
-        
+
         if initial:
             bookmark_form = BookmarkInstanceForm(initial=initial)
         else:
             bookmark_form = BookmarkInstanceForm()
-    
+
     bookmarks_add_url = "http://" + Site.objects.get_current().domain + reverse(add)
     bookmarklet = "javascript:location.href='%s?url='+encodeURIComponent(location.href)+';description='+encodeURIComponent(document.title)+';redirect=on'" % bookmarks_add_url
-    
+
     return render_to_response("bookmarks/add.html", {
         "bookmarklet": bookmarklet,
         "bookmark_form": bookmark_form,
@@ -91,16 +109,15 @@ def add(request):
 
 @login_required
 def delete(request, bookmark_instance_id):
-    
+
     bookmark_instance = get_object_or_404(BookmarkInstance, id=bookmark_instance_id)
     if request.user == bookmark_instance.user:
         bookmark_instance.delete()
         request.user.message_set.create(message="Bookmark Deleted")
-        
+
     if "next" in request.GET:
         next = request.GET["next"]
     else:
         next = reverse("bookmarks.views.bookmarks")
-    
+
     return HttpResponseRedirect(next)
-    
