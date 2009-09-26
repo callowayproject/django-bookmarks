23bcb597938b26bf110a14c9b69d3696c09642f7
kevin fricovsky <kevin@montylounge.com>
2009-09-27 04:01:25 +0800

    added detail view url

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -1,20 +1,12 @@
 from django.conf.urls.defaults import *
-
-# for voting
-from voting.views import vote_on_object
 from bookmarks.models import Bookmark
 
 urlpatterns = patterns('',
     url(r'^$', 'bookmarks.views.bookmarks', name="all_bookmarks"),
+    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$',
+        view='bookmarks.views.bookmark_detail',
+        name='bookmark_detail'),
     url(r'^your_bookmarks/$', 'bookmarks.views.your_bookmarks', name="your_bookmarks"),
     url(r'^add/$', 'bookmarks.views.add', name="add_bookmark"),
     url(r'^(\d+)/delete/$', 'bookmarks.views.delete', name="delete_bookmark_instance"),
-    
-    # for voting
-    (r'^(?P<object_id>\d+)/(?P<direction>up|down|clear)vote/?$',
-        vote_on_object, dict(
-            model=Bookmark,
-            template_object_name='bookmark',
-            template_name='kb/link_confirm_vote.html',
-            allow_xmlhttprequest=True)),
 )
