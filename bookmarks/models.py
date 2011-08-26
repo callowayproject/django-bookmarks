2d2c10a49836c0a15880b4e23f140c4e65d6642b
Corey Oordt <coordt@washingtontimes.com>
2011-08-26 15:31:37 -0400

    Added setting UNIQUE_BOOKMARKS

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -15,7 +15,8 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 from django.core.exceptions import ImproperlyConfigured
 
-from settings import VERIFY_EXISTS, USE_TAGGING, ABSOLUTE_URL_IS_BOOKMARK
+from settings import (VERIFY_EXISTS, USE_TAGGING, ABSOLUTE_URL_IS_BOOKMARK, 
+                      UNIQUE_BOOKMARKS)
 
 if USE_TAGGING:
     try:
@@ -28,17 +29,27 @@ if USE_TAGGING:
 
 class Bookmark(models.Model):
     
-    url = models.URLField(unique=True)
-    description = models.CharField(_('description'), max_length=100)
+    url = models.URLField(unique=UNIQUE_BOOKMARKS)
+    description = models.CharField(
+        _('description'), 
+        max_length=100)
     slug = models.SlugField()
-    note = models.TextField(_('note'), blank=True)
-    
+    note = models.TextField(
+        _('note'), 
+        blank=True)
     has_favicon = models.BooleanField(_('has favicon'))
-    favicon_checked = models.DateTimeField(_('favicon checked'), default=datetime.now)
-    
-    adder = models.ForeignKey(User, blank=True, null=True,
-                    related_name="added_bookmarks", verbose_name=_('adder'))
-    added = models.DateTimeField(_('added'), default=datetime.now)
+    favicon_checked = models.DateTimeField(
+        _('favicon checked'), 
+        default=datetime.now)
+    adder = models.ForeignKey(
+        User,
+        blank=True, 
+        null=True,
+        related_name="added_bookmarks", 
+        verbose_name=_('adder'))
+    added = models.DateTimeField(
+        _('added'), 
+        default=datetime.now)
     
     if USE_TAGGING:
         tags = TagField()
@@ -61,13 +72,17 @@ class Bookmark(models.Model):
     
     def all_tags(self, min_count=False):
         if USE_TAGGING:
-            return Tag.objects.usage_for_model(BookmarkInstance, counts=False, min_count=None, filters={'bookmark': self.id})
+            return Tag.objects.usage_for_model(
+                BookmarkInstance, counts=False, min_count=None, 
+                filters={'bookmark': self.id})
         else:
             return self.tags.split(',')
     
     def all_tags_with_counts(self, min_count=False):
         if USE_TAGGING:
-            return Tag.objects.usage_for_model(BookmarkInstance, counts=True, min_count=None, filters={'bookmark': self.id})
+            return Tag.objects.usage_for_model(
+                BookmarkInstance, counts=True, min_count=None, 
+                filters={'bookmark': self.id})
         else:
             return self.tags.split(',')
     
@@ -86,7 +101,7 @@ class Bookmark(models.Model):
                 'day': self.added.day,
                 'slug': self.slug
             })
-        
+    
     class Meta:
         ordering = ('-added', )
 
@@ -98,25 +113,39 @@ class BookmarkInstance(models.Model):
     
     Its admin will not show, however, if MULTIUSER is False
     """
+    url = models.URLField(unique=False)
+    bookmark = models.ForeignKey(
+        Bookmark, 
+        related_name="saved_instances", 
+        verbose_name=_('bookmark'))
+    user = models.ForeignKey(
+        User, 
+        related_name="saved_bookmarks", 
+        verbose_name=_('user'))
+    saved = models.DateTimeField(
+        _('saved'), 
+        default=datetime.now)
+    description = models.CharField(
+        _('description'), 
+        max_length=100)
+    note = models.TextField(
+        _('note'), 
+        blank=True)
     
-    bookmark = models.ForeignKey(Bookmark, related_name="saved_instances", verbose_name=_('bookmark'))
-    user = models.ForeignKey(User, related_name="saved_bookmarks", verbose_name=_('user'))
-    saved = models.DateTimeField(_('saved'), default=datetime.now)
-    
-    description = models.CharField(_('description'), max_length=100)
-    note = models.TextField(_('note'), blank=True)
-    
-    #tags = TagField()
-    
-    def save(self, force_insert=False, force_update=False, edit=False):
+    def save(self, force_insert=False, force_update=False, edit=False, **kwargs):
+        from django.template.defaultfilters import slugify
         if edit:
             super(BookmarkInstance, self).save(force_insert, True)
         else:
             try:
                 bookmark = Bookmark.objects.get(url=self.url)
             except Bookmark.DoesNotExist:
-                # has_favicon=False is temporary as the view for adding bookmarks will change it
-                bookmark = Bookmark(url=self.url, description=self.description, note=self.note, has_favicon=False, adder=self.user)
+                # has_favicon=False is temporary as the view for adding 
+                # bookmarks will change it
+                bookmark = Bookmark(
+                    url=self.url, slug=slugify(self.description), 
+                    description=self.description, note=self.note, 
+                    has_favicon=False, adder=self.user)
                 bookmark.save()
             self.bookmark = bookmark
             super(BookmarkInstance, self).save(force_insert, force_update)
@@ -128,4 +157,6 @@ class BookmarkInstance(models.Model):
             bookmark.delete()
     
     def __unicode__(self):
-        return _("%(bookmark)s for %(user)s") % {'bookmark':self.bookmark, 'user':self.user}
+        return _("%(bookmark)s for %(user)s") % {
+            'bookmark':self.bookmark, 'user':self.user
+        }
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -6,6 +6,7 @@ DEFAULT_SETTINGS = {
     'ITEMS_PER_FEED': 20,
     'ABSOLUTE_URL_IS_BOOKMARK': True,
     'MULTIUSER': True,
+    'UNIQUE_BOOKMARKS': True
 }
 
 DEFAULT_SETTINGS.update(getattr(settings, 'BOOKMARK_SETTINGS', {}))
